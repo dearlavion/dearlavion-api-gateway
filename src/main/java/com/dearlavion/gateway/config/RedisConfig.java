d88ce3ba290ca17faf592d5b52dbf1b6218da25b
alysson <dearlavion@gmail.com>
2026-03-27 23:02:47 +0800

    fix ratelimiter

--- src/main/java/com/dearlavion/gateway/config/RedisConfig.java
+++ src/main/java/com/dearlavion/gateway/config/RedisConfig.java
@@ -1,4 +1,20 @@
 package com.dearlavion.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
 public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        System.out.println("[Gateway Redis] host: " + host);
+        System.out.println("[Gateway Redis] port: " + port);
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
 }
